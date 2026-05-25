56694444542d42abfba29f936177ab7ad10edd70
Andrew Rios <andrewriosc@icloud.com>
2026-05-25 11:13:24 -0700

    Add enemy health and bullet damage

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,8 @@ public class Bullet : MonoBehaviour
     private Vector3 direction;
     private float speed;
 
+    public int damage = 1;
+
     public void Initialize(Vector3 direction, float speed, float lifetime)
     {
         this.direction = direction;
@@ -15,5 +17,16 @@ public class Bullet : MonoBehaviour
     void Update()
     {
         transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EnemyHealth enemy = other.GetComponent<EnemyHealth>();
+
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
